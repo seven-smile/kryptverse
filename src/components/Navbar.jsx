65e4412116c72e39e8365a128bbf15f839550ecd
Nana <juniornyamile@gmail.com>
2022-06-13 10:29:01 +0000

    index

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import {Button, Menu,Typography, Avatar} from 'antd';
+import {Link} from 'react-router-dom';
+import { HomeOutlined, MoneyCollectionOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons';
+
+import icon from '../images/cryptocurrency.png'
+   
+const Navbar = () => {
+  return (
+    <div className='nav-container'>
+        <div className='logo-container'>
+            <Avatar src={icon} size="large" />
+            <Typography.Title level={2} className="logo">
+                <Link to="/">Kryptoverse</Link>
+            </Typography.Title>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
