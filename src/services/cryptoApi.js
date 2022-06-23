1e25767ecf260f40a178acc07943fd01ebf2347a
Nana <juniornyamile@gmail.com>
2022-06-23 16:48:28 +0000

    Api

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,34 @@
+import { createApi,fetchBaseQuery} from "@reduxjs/toolkit/query";
+
+const cryptoApiHeaders = {
+    'X-RapidAPI-Key': '7fac198fc7msh3fefa83f3b473bap14f61ejsn0143a2d4deae',
+    'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com/coins';
+
+// const axios = require("axios");
+
+// const options = {
+//   method: 'GET',
+//   url: 'https://coinranking1.p.rapidapi.com/coins',
+//   params: {
+//     referenceCurrencyUuid: 'yhjMzLPhuIDl',
+//     timePeriod: '24h',
+//     'tiers[0]': '1',
+//     orderBy: 'marketCap',
+//     orderDirection: 'desc',
+//     limit: '50',
+//     offset: '0'
+//   },
+//   headers: {
+//     'X-RapidAPI-Key': '7fac198fc7msh3fefa83f3b473bap14f61ejsn0143a2d4deae',
+//     'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+//   }
+// };
+
+// axios.request(options).then(function (response) {
+// 	console.log(response.data);
+// }).catch(function (error) {
+// 	console.error(error);
+// });
